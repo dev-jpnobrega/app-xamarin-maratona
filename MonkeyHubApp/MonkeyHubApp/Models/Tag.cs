fcecec171ce997415ef2b5d7177354298577201c
Joao Paulo <joaopaulonobrega@hotmail.com>
2017-05-04 16:10:57 -0300

    Teste branch

--- MonkeyHubApp/MonkeyHubApp/Models/Tag.cs
+++ MonkeyHubApp/MonkeyHubApp/Models/Tag.cs
@@ -6,5 +6,6 @@ namespace MonkeyHubApp.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public bool active { get; set; }
     }
 }
